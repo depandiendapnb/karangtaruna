3a14f753e19044a6603d08d27ce05f1ae7763c89
Depandi Enda <depandienda@gmail.com>
2022-08-16 20:18:55 +0700

    hapus hello world

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php require_once('header.php'); ?>
 
 <?php
-echo "<h1>Hello World Depandi Enda</h1>";
 $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
 $statement->execute();
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
